03948a0f6974d18baf6a2d332bdf684ba6c54287
Tobias G <31646748+Daikalos@users.noreply.github.com>
2020-01-04 22:23:12 +0100

    *performance quick fix*

--- Tower-Defense/Tower-Defense/Game/Main/UserInterface.cs
+++ Tower-Defense/Tower-Defense/Game/Main/UserInterface.cs
@@ -15,16 +15,18 @@ namespace Tower_Defense
         private static Vector2 myPosition;
         private static Point mySize;
         private static Rectangle myDestRect;
+        private static Color[] myPixels;
 
         public static void Initialize(Vector2 aPosition, Point aSize, GraphicsDevice aGraphicsDevice)
         {
-            myPosition = aPosition;
-            mySize = aSize;
-            myDestRect = new Rectangle((int)myPosition.X, (int)myPosition.Y, mySize.X, mySize.Y);
-
             myDevice = aGraphicsDevice;
             mySpriteBatch = new SpriteBatch(aGraphicsDevice);
             myInterface = new RenderTarget2D(aGraphicsDevice, aGraphicsDevice.Viewport.Width, aGraphicsDevice.Viewport.Height);
+
+            myPosition = aPosition;
+            mySize = aSize;
+            myDestRect = new Rectangle((int)myPosition.X, (int)myPosition.Y, mySize.X, mySize.Y);
+            myPixels = new Color[myInterface.Width * myInterface.Height];
         }
 
         public static void Draw(SpriteBatch aSpriteBatch)
@@ -37,12 +39,11 @@ namespace Tower_Defense
 
         public static bool IsMouseOutside()
         {
-            if (KeyMouseReader.MousePos.X >= 0 && KeyMouseReader.MousePos.Y >= 0 && KeyMouseReader.MousePos.X <= myInterface.Width && KeyMouseReader.MousePos.Y <= myInterface.Height)
+            if (KeyMouseReader.MousePos.X >= 0 && KeyMouseReader.MousePos.Y >= 0 && KeyMouseReader.MousePos.X < myInterface.Width && KeyMouseReader.MousePos.Y < myInterface.Height)
             {
-                Color[] tempPixels = new Color[myInterface.Width * myInterface.Height];
-                myInterface.GetData(0, new Rectangle((int)KeyMouseReader.MousePos.X, (int)KeyMouseReader.MousePos.Y, 1, 1), tempPixels, 0, 1);
+                myInterface.GetData(0, new Rectangle((int)KeyMouseReader.MousePos.X, (int)KeyMouseReader.MousePos.Y, 1, 1), myPixels, 0, 1);
 
-                return tempPixels[0].A < 10;
+                return myPixels[0].A < 10;
             }
             return false;
         }
